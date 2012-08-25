f7868d83f6b9cac2be0165cf84a5cbafa9c24e5c
Axel Rutz <axel.rutz@clever-systems.net>
2012-08-25 17:25:40 +0200

    moved infostore in example-infostore dir

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from specstore import InfoStore
+from infostore import InfoStore
 from pprint import pprint
 import os
 
-s=InfoStore(os.path.dirname(__file__))
+s=InfoStore(os.path.dirname(__file__)+'/example-infostore')
 pprint(s)
